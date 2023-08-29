3fcac886d1474a42d9bd4694a0798fa862cfbccd
henzoparahua <hnz.2894henzop19@gmail.com>
2023-08-29 19:53:56 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/MyViewPageAdapter.java
@@ -0,0 +1,33 @@
+package br.com.etecia.myapp;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class MyViewPageAdapter extends FragmentStateAdapter {
+
+
+    public MyViewPageAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        switch (position){
+            case 0:
+                return  new dogFragment();
+            case 1:
+                return  new catsFragment();
+            default:
+                return new snakeFragment();
+        }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
